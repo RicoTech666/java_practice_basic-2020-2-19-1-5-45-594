a25008051123f2bd1004af9dd8913a13a9ed40c1
RicoTech666 <zhoujiachenbe@gmail.com>
2020-02-19 12:01:46 +0800

    [Jiachen] feat: add classes

--- map/src/main/java/Application.java
+++ map/src/main/java/Application.java
@@ -1,6 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Application {
 
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        Student stu1 = new Student("Linda", 18);
+        Student stu2 = new Student("Cindy", 19);
+        Student stu3 = new Student("Mary", 19);
+        List<Student> studentList1 = new ArrayList<>();
+        List<Student> studentList2 = new ArrayList<>();
+        studentList1.add(stu1);
+        studentList2.add(stu2);
+        studentList2.add(stu3);
+
+        Map<Integer, List<Student>> students = new HashMap<>();
+        students.put(stu1.getAge(), studentList1);
+        students.put(stu2.getAge(), studentList2);
+
+        System.out.println("Before adding new students:");
+        Teacher teacher = new Teacher("Bob", students);
+
+        teacher.printStudentInfo();
+
+        teacher.addStudent(new Student("Ben", 19));
+        teacher.addStudent(new Student("Solider", 20));
+
+        System.out.println("After adding new students: ");
+        teacher.printStudentInfo();
+    }
 
 }
--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Teacher {
 
+    private String name;
+    private Map<Integer, List<Student>> students;
+
+    public Teacher(String name, Map<Integer, List<Student>> students) {
+        this.name = name;
+        this.students = students;
+    }
+
+    public void addStudent(Student student) {
+        int studentAge = student.getAge();
+        if (students.containsKey(studentAge)) {
+            List<Student> studentList = students.get(studentAge);
+            studentList.add(student);
+            students.replace(studentAge, studentList);
+        } else {
+            List<Student> studentList = new ArrayList<>();
+            studentList.add(student);
+            students.put(studentAge, studentList);
+        }
+    }
+
+    public void printStudentInfo() {
+        System.out.println(students);
+    }
 }
